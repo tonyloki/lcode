2a8d3e63e8d5c15af85219e5a9b019acbe0f8099
Logesh E <logesh090707@gmail.com>
2026-03-01 08:44:03 +0000

    Sync LeetCode submission - Partitioning Into Minimum Number Of Deci-Binary Numbers (java)

---
+++ solutions/problems/partitioning_into_minimum_number_of_deci-binary_numbers/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minPartitions(String n) {
+
+        int maxDigit = 0;
+
+        for (char c : n.toCharArray()) {
+            maxDigit = Math.max(maxDigit, c - '0');
+
+            if (maxDigit == 9) return 9; // early exit
+        }
+
+        return maxDigit;
+    }
+}
