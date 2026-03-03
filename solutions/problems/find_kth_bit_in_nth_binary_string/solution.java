0a92d8f54b6e9f389a2b39c5d85208d881777d48
Logesh E <logesh090707@gmail.com>
2026-03-03 12:59:14 +0000

    Sync LeetCode submission - Find Kth Bit in Nth Binary String (java)

---
+++ solutions/problems/find_kth_bit_in_nth_binary_string/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public char findKthBit(int n, int k) {
+        if (n == 1) return '0';
+
+        int length = (1 << n) - 1;
+        int mid = (length / 2) + 1;
+
+        if (k == mid) return '1';
+
+        if (k < mid)
+            return findKthBit(n - 1, k);
+
+        // mirror position
+        int mirror = length - k + 1;
+
+        char bit = findKthBit(n - 1, mirror);
+
+        // invert
+        return bit == '0' ? '1' : '0';
+    }
+}
