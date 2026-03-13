a0e9279bf65e24c6fac4b60e59898ea623787ae2
Logesh E <logesh090707@gmail.com>
2026-03-13 17:17:27 +0000

    Sync LeetCode submission - Minimum Number of Seconds to Make Mountain Height Zero (java)

---
+++ solutions/problems/minimum_number_of_seconds_to_make_mountain_height_zero/solution.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+
+        long left = 0;
+        long right = (long)1e18;
+        long ans = right;
+
+        while (left <= right) {
+
+            long mid = (left + right) / 2;
+
+            if (can(mid, mountainHeight, workerTimes)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    boolean can(long time, int height, int[] workers) {
+
+        long total = 0;
+
+        for (int t : workers) {
+
+            long val = (2 * time) / t;
+
+            long x = (long)((Math.sqrt(1 + 4 * val) - 1) / 2);
+
+            total += x;
+
+            if (total >= height)
+                return true;
+        }
+
+        return total >= height;
+    }
+}
