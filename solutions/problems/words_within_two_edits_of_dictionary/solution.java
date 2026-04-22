65506f345fd8f9d6fbaa9ace165721ec5ec17b97
Logesh E <logesh090707@gmail.com>
2026-04-22 13:57:34 +0000

    Sync LeetCode submission - Words Within Two Edits of Dictionary (java)

---
+++ solutions/problems/words_within_two_edits_of_dictionary/solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList<>();
+        
+        for (String q : queries) {
+            for (String d : dictionary) {
+                if (isValid(q, d)) {
+                    result.add(q);
+                    break; // no need to check further
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    private boolean isValid(String a, String b) {
+        int diff = 0;
+        
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diff++;
+                if (diff > 2) return false;
+            }
+        }
+        
+        return true;
+    }
+}
