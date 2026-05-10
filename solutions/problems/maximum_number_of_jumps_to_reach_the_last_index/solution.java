1745fb4d902ce346abb234cdeb42c782b8193220
Logesh E <logesh090707@gmail.com>
2026-05-10 08:07:17 +0000

    Sync LeetCode submission - Maximum Number of Jumps to Reach the Last Index (java)

---
+++ solutions/problems/maximum_number_of_jumps_to_reach_the_last_index/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+        int n = nums.length;
+        
+        int[] dp = new int[n];
+        
+        // initialize with impossible value
+        for (int i = 0; i < n; i++) {
+            dp[i] = -1;
+        }
+        
+        dp[0] = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == -1) continue;
+            
+            for (int j = i + 1; j < n; j++) {
+                long diff = (long) nums[j] - nums[i];
+                
+                if (Math.abs(diff) <= target) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+        
+        return dp[n - 1];
+    }
+}
