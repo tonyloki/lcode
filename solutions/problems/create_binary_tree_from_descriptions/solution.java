b149170e42721ea7907a01639681d1ca6cffc7e6
Logesh E <logesh090707@gmail.com>
2026-06-07 18:09:04 +0000

    Sync LeetCode submission - Create Binary Tree From Descriptions (java)

---
+++ solutions/problems/create_binary_tree_from_descriptions/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> map = new HashMap<>();
+        Set<Integer> children = new HashSet<>();
+
+        for (int[] d : descriptions) {
+            int parent = d[0];
+            int child = d[1];
+            int isLeft = d[2];
+
+            map.putIfAbsent(parent, new TreeNode(parent));
+            map.putIfAbsent(child, new TreeNode(child));
+
+            TreeNode parentNode = map.get(parent);
+            TreeNode childNode = map.get(child);
+
+            if (isLeft == 1) {
+                parentNode.left = childNode;
+            } else {
+                parentNode.right = childNode;
+            }
+
+            children.add(child);
+        }
+
+        for (int[] d : descriptions) {
+            int parent = d[0];
+            if (!children.contains(parent)) {
+                return map.get(parent);
+            }
+        }
+
+        return null;
+    }
+}
