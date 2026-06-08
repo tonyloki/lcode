a6e6305253de130560723024d18fffcf984c575f
Logesh E <logesh090707@gmail.com>
2026-06-08 13:30:34 +0000

    Sync LeetCode submission - Partition Array According to Given Pivot (java)

---
+++ solutions/problems/partition_array_according_to_given_pivot/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int idx = 0;
+        for (int num : nums) {
+            if (num < pivot) {
+                ans[idx++] = num;
+            }
+        }
+        for (int num : nums) {
+            if (num == pivot) {
+                ans[idx++] = num;
+            }
+        }
+        for (int num : nums) {
+            if (num > pivot) {
+                ans[idx++] = num;
+            }
+        }
+
+        return ans;
+    }
+}
