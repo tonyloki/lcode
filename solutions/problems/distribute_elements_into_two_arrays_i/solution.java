112003e716ecbb81b116ecc1a243776c903ed0f6
Logesh E <logesh090707@gmail.com>
2026-08-20 12:36:50 +0000

    Sync LeetCode submission - Distribute Elements Into Two Arrays I (java)

---
+++ solutions/problems/distribute_elements_into_two_arrays_i/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        int size1 = 0;
+        int size2 = 0;
+
+        // First operation
+        arr1[size1++] = nums[0];
+
+        // Second operation
+        arr2[size2++] = nums[1];
+
+        // Remaining elements
+        for (int i = 2; i < n; i++) {
+            if (arr1[size1 - 1] > arr2[size2 - 1]) {
+                arr1[size1++] = nums[i];
+            } else {
+                arr2[size2++] = nums[i];
+            }
+        }
+
+        // Concatenate arr1 and arr2
+        int[] result = new int[n];
+
+        for (int i = 0; i < size1; i++) {
+            result[i] = arr1[i];
+        }
+
+        for (int i = 0; i < size2; i++) {
+            result[size1 + i] = arr2[i];
+        }
+
+        return result;
+    }
+}
