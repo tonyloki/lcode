910d11436282e34fd2f73bbc873c58d60f925c0a
logesh090707@gmail.com <logesh090707@gmail.com>
2026-09-15 12:40:00 +0000

    [LeetCode Sync] Runtime - 134 ms (22.61%), Memory - 62.4 MB (11.56%)

---
+++ solutions/2559-maximum-number-of-non-overlapping-palindrome-substrings/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxPalindromes(String s, int k) {
+        int n = s.length();
+
+        boolean[][] pal = new boolean[n][n];
+
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j) &&
+                    (len <= 2 || pal[i + 1][j - 1])) {
+                    pal[i][j] = true;
+                }
+            }
+        }
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1];
+
+            for (int j = 0; j < i; j++) {
+                if (i - j >= k && pal[j][i - 1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
